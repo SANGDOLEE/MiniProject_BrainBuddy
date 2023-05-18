3bc46fc8c74248c66d7dae87cba3ac12a1017880
SANGDOLEE <xkdxh625@naver.com>
2023-05-19 01:41:00 +0900

    label, imageView -> Snapkit

--- StudyMemoryTest/Controller/AddImageViewController.swift
+++ StudyMemoryTest/Controller/AddImageViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import AVFoundation
 import Photos
+import Vision
 
 class AddImageViewController: UIViewController {
     
-    private var addImageView : AddImageView!
+    private var addImageView : AddImageView! // View
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,10 @@ class AddImageViewController: UIViewController {
         
         navigationItem.rightBarButtonItems = [ saveButton, addCameraButton ] // 네비게이션 버튼2개 배열로 할당
         
+        
+        addImageView.setImage(UIImage(named: "example1"))
+        recognizerText(image: addImageView.imageView.image)
+        
         /*
          let navigationBar = UINavigationBar(frame: CGRect(x:0, y:0, width:view.frame.size.width, height:view.frame.size.height))
          navigationBar.backgroundColor = .systemBlue
@@ -41,6 +46,48 @@ class AddImageViewController: UIViewController {
          */
         
     }
+    
+    // MARK: Visision - Image -> Text
+    private func recognizerText(image: UIImage?) { // 이미지는 선택사항
+        guard let cgImage = image?.cgImage else { return } // Ui 이미지의 CG이미지 버전을 얻는 것
+        
+        // Handler
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        
+        // Reqeust ( 텍스트 감지를 위한 요청 생성 )
+        let request = VNRecognizeTextRequest{ [weak self] request, error in
+            // 실제 관찰된 정보 가져오기 ( request.results = 관찰 / 여기에서 문자열을 가져오고 )
+            guard let observations = request.results as? [VNRecognizedTextObservation],
+                  error == nil else {
+                return
+            }
+            
+            // 압축매핑
+            let text = observations.compactMap({
+                $0.topCandidates(1).first?.string
+            }).joined(separator:"\n") // 줄바꿈으로 구분
+            
+            DispatchQueue.main.async {
+                self?.addImageView.setLabelText(text)
+            }
+        }
+        
+        // 텍스트 감지 언어 -> 한국어, 영어
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = ["ko","en"]
+        request.usesLanguageCorrection = true
+        request.revision = VNRecognizeTextRequestRevision3
+        
+        // Process request
+        do {
+            try handler.perform([request]) // Handler에서 요청 수행
+        }
+        catch {
+            addImageView.setLabelText("\(error)")
+        }
+        
+    }
+    
     
     // 카메라 선택
     @objc func addCameraTapped() {
@@ -74,7 +121,20 @@ class AddImageViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
         
     }
-    // 카메라 Permission 함수
+    
+    // Camera -> 사진 추가 or 추
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            addImageView.imageView.image = image
+            recognizerText(image: image)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // 카메라 Permission
     func checkCameraPermissions() {
         AVCaptureDevice.requestAccess(for: .video, completionHandler: { (granted: Bool) in
             if granted {
@@ -85,6 +145,7 @@ class AddImageViewController: UIViewController {
             
         })
     }
+    // 앨범 Permission
     func checkAlbumPermission() {
         PHPhotoLibrary.requestAuthorization( { status in
             switch status {
@@ -103,15 +164,6 @@ class AddImageViewController: UIViewController {
     @objc func saveTapped() {
         
     }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
 
--- StudyMemoryTest/View/AddImageView.swift
+++ StudyMemoryTest/View/AddImageView.swift
@@ -6,21 +6,63 @@
 //
 
 import UIKit
+import SnapKit
 
 class AddImageView : UIView{
     
+    let label: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.text = "Starting..."
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named:"example1")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override init(frame: CGRect){
-        super.init(frame: frame)
-        
-        // 배경색 설정
+        super.init(frame:frame)
+        addSubview(label)
+        addSubview(imageView)
         self.backgroundColor = UIColor.systemBlue
     }
     
-    required init?(coder aDecoder: NSCoder){
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        fatalError("init(coder: has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+      
+        imageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(safeAreaInsets.top)
+            make.left.equalToSuperview().offset(20)
+            make.width.equalTo(frame.size.width - 40)
+            make.height.equalTo(frame.size.width - 40)
+        }
         
-        //배경색 설정
-        // self.backgroundColor = UIColor.systemBlue
+        label.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(frame.size.width + safeAreaInsets.top)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(200)
+        }
+    }
+    
+    func setImage(_ image: UIImage?){
+        imageView.image = image
+    }
+    
+    func setLabelText(_ text: String) {
+        label.text = text
     }
     
     
